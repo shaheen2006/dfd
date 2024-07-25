cb5cd0983de7af2a2b93a3ad90c91b9b11ab253e
Mohammed Shaheen <160334317+shaheen2006@users.noreply.github.com>
2024-07-25 11:52:54 +0530

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,14 @@
+#to find Area of the Triangle
+print("Find Area of Triangle")
+print()
+
+#input function
+b = int(input("Enter the Base : "))
+h = int(input("Enter the Height : "))
+print()
+
+#function
+Triangle = 1/2*b*h
+
+#output function
+print("The base {} and height {} of Triangle AREA is {}".format(b,h,Triangle))
